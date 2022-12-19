65ab84858616ee6603b1bce31e03f6609380f58c
hafiz-aa <mhafiz.aa@gmail.com>
2022-12-19 11:08:49 +0700

    Fetch data from reqres API

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-console.log("Hello")
+const endPoint = "https://reqres.in/api/users"
+
+// fetch(endPoint)
+// 	.then(result => result.json())
+// 	.then(({ data }) => console.log(data))
+
+async function hitAPI() {
+	const api = await fetch(endPoint)
+	const { data } = await api.json()
+	console.log(data)
+}
+
+hitAPI()
