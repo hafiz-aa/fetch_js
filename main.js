600dcfa538553b8d1d54ca55bcc26938ad94f433
hafiz-aa <mhafiz.aa@gmail.com>
2022-12-19 12:39:16 +0700

    fetch post

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
 const endPoint = "https://reqres.in/api/users"
 
-// fetch(endPoint)
-// 	.then(result => result.json())
-// 	.then(({ data }) => console.log(data))
+fetch(endPoint, {
+	method: "POST",
+	headers: {
+		"Content-Type": "application/json",
+	},
+	body: JSON.stringify({
+		email: "hafiz@gmail.com",
+		firstName: "Hafiz"
+	}),
+})
+	.then((result) => result.json())
+	.then((data) => console.log(data))
 
-async function hitAPI() {
-	const api = await fetch(endPoint)
-	const { data } = await api.json()
-	console.log(data)
-}
+// async function hitAPI() {
+// 	const api = await fetch(endPoint)
+// 	const { data } = await api.json()
+// 	console.log(data)
+// }
 
-hitAPI()
+// hitAPI()
